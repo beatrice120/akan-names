face4933a90646058717b4c78c2a00abd6ca136e
beatrice120 <beatrice120>
2020-09-26 20:35:04 +0200

    javascript validation

--- js/script.js
+++ js/script.js
@@ -1,69 +1,36 @@
 
 var send= function() {
-  var daysOfTheWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  var maleNames = [
-    "Kwasi",
-    "Kwadwo",
-    "Kwabena",
-    "Kwaku",
-    "Yaw",
-    "Kofi",
-    "Kwame"
-  ];
-  var femaleNames = [
-    "Akosua",
-     " Adwoa",
-    "Abenaa",
-    "Akua",
-    "Yaa",
-    "Afua",
-    "Ama"
-  ];
-  var month = parseInt(document.getElementById("month").value);
   var year = parseInt(document.getElementById("year").value);
   var day = parseInt(document.getElementById("day").value);
   var name =(document.getElementById("name").value);
+  var month = parseInt(document.getElementById("month").value);
   var date0fbirth = new Date(year + "/" + month + "/" + day);
   var results = date0fbirth.getDay();
-  var output = document.getElementById("output");
-  var male = document.getElementById("male")
+   var male = document.getElementById("male") 
   var female = document.getElementById("female")
-  
-  if (month =="" && year =="" && day =="" && name=="") {
-    alert("Please Enter you credentials");
+  var output = document.getElementById("return");
+  var daysOfTheWeek = [ "Sunday","Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday" ];
+ var maleNames = ["Kwasi","Kwadwo", "Kwabena","Kwaku", "Yaw", "Kofi", "Kwame"];
+ var femaleNames = [ "Akosua"," Adwoa", "Abenaa",  "Akua",  "Yaa",  "Afua", "Ama" ];
+ 
+if (month =="" && year =="" && day =="" && name=="") {
+    prompt("enter your informations");
     return false;
   }
 
-//   if (month == null || month== "", day == null || day == "", year== null || year == "", name == null || name == "") {
-//     alert("Please Fill All Required Field");
-//     return false;
-//   }
-// }
-  
-  
- if (year < 0) {
+  if (year < 0) {
    
-    output.innerHTML = "Hey! " + name + " please enter a valid year! "
+    output.innerHTML = "dear " + name + "  enter a valid year! "
 }
   
-else if ((month < 1) || (month > 31)) {
-    output.style.background ="red"
-    output.style.color= "white"
-    output.innerHTML = "Hey! " + name + " please enter a valid month! "
+else if ((month < 1) || (month >= 31)) {
+    
+    output.innerHTML = "dear! " + name + " enter a valid month ! "
 }
   
- else if (day < 0 || day > 31) {
-    output.style.background ="red"
-    output.style.color= "white"
-    output.innerHTML = "Hey! " + name + " please enter a valid day! "
+ else if (day <= 0 || day >=31) {
+    
+    output.innerHTML = "dear " + name + " enter your valid day! "
 }
 
   if(male.checked && year > 0 && month > 0 && month < 12 && day > 0 && day < 32) {
